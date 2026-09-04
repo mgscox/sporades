e6befe515045830b05f56c07f8015e898cac4887
Matt Cox <mattgscox@hotmail.com>
2026-09-04 14:29:29 +0100

    Release v0.9.15

--- bin/sporades.js
+++ bin/sporades.js
@@ -110609,7 +110609,7 @@ jobs:
 }
 
 // src/cli/cli-version.ts
-var CLI_VERSION = "0.9.14";
+var CLI_VERSION = "0.9.15";
 
 // src/cli/sporades.ts
 var SUPPORTED_TEMPLATES = new Set(CLIENT_TEMPLATES);
--- dist/cli/cli-version.d.ts
+++ dist/cli/cli-version.d.ts
@@ -1,2 +1,2 @@
-export declare const CLI_VERSION = "0.9.14";
+export declare const CLI_VERSION = "0.9.15";
 //# sourceMappingURL=cli-version.d.ts.map
